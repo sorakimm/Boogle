be1a10ef98b1195f51182cd7f8fd5db8909e3d31
sorak17 <sorak17@gmail.com>
2016-03-30 10:15:56 +0900

    Changed the HTML for the site.

--- search/search_views.py
+++ search/search_views.py
@@ -1,25 +1,32 @@
-#-*- coding:cp949
-from django.shortcuts import render
-from Searcher import c_searcher
+#-*- coding: cp949 -*-
 from django.http import HttpResponse
 
 #for template
 from django.template import Context, loader
+from SubCrawler import *
+from Searcher import SubSearcher, DictSearcher, WebSearcher
+from Searcher import *
+import os
+
+#global value
 
 listSize = 10
 showLength = 100
 
-# Create your views here.
-def main_page(request):
-    return render(request, 'search/main_page.html', {})
+def ParameterInputError(req):
+    html = "error"
+    return HttpResponse(html)
 
+#def search_page(request):
+#    return render(request, 'search/search.html', {})
+
+   
 
 def SearchPage(req, mode, keyword, page=1): # req : request
     global listSize
-    #mode = 'smisearch'
-    #keyword = 'good'
+    mode = 'smisearch'
+    keyword = 'good'
     page = int(page)
-    print (page)
     #if(mode == smisearch):
     #    resultTup = callSMISearch(keyword)
     #elif(mode == dictsearch):
@@ -28,19 +35,16 @@ def SearchPage(req, mode, keyword, page=1): # req : request
     #    resultTup = callWEBSearch(keyword)
     resultTup = callSearch(mode, keyword)
     showItemTup = resultTup[((page-1)*listSize):(page*listSize)] # 한번에 열개씩
-    
     searchList = matchContentData(mode, keyword, showItemTup) # 페이지에 표시할 데이터 생성
-    #print(searchList)
+    
 
     totalSize = len(resultTup) # 검색결과 밑 네비게이터 부분
-    pageCnt = int(totalSize / listSize)
-    if(totalSize % listSize) != 0:
+    pageCnt = totalSize / listSize
+    if(totalSize % listSIze) != 0:
         pageCnt += 1
     pageList = range(1, pageCnt+1)
-    if mode == 'smisearch':
-        tpl = loader.get_template('search/sub_search.html')
-    else:
-        tpl = loader.get_template('search/search.html') # 템플릿 로딩
+
+    tpl = loader.get_template('search/search.html') # 템플릿 로딩
     ctx = Context({
         'searchList' : searchList, # 변수값 채우기
         'keyword' : keyword,
@@ -52,27 +56,25 @@ def SearchPage(req, mode, keyword, page=1): # req : request
     return HttpResponse(html)
 
 def callSearch(mode, keyword):
-    searcher = c_searcher(keyword.encode('cp949'))
     if mode == "smisearch":
-        searchTup = searcher.SubSearcher()
-        
-    #elif mode == "dictsearch":
-    #    rfunc = DictSearcher(keyword)
-    #elif mode == "websearch":
-    #    rfunc = WebSearcher(keyword)
+        rfunc = SubSearcher(keyword)
+    elif mode == "dictsearch":
+        rfunc = DictSearcher(keyword)
+    elif mode == "websearch":
+        rfunc = WebSearcher(keyword)
 
+    searchTup = rfunc(keyword.encode('cp949'))
     return searchTup
 
 def matchContentData(mode, keyword, listTup):
-    #"템플릿에 사용될 데이터 생성 "
+    "템플릿에 사용될 데이터 생성 "
     ResultData = []
     if(mode == "smisearch"):
         for item in listTup:
-            conTitle = item[0].replace(item[0], "<b>" + item[0] + "</b>")
+            conTitle = item[0]
             conEng = item[1]
             conKor = item[2]
             ResultData.append(({'title':conTitle, 'eng':conEng, 'kor':conKor}))
-            #print (ResultData)
     return ResultData
 
     if(mode == "dictsearch" | mode == "websearch"):
@@ -97,7 +99,7 @@ def makeContentPreview(keyword, text):
         PreviewData = contData[0:showLength]
 
     if len(PreviewData) <= 0:
-        PreviewData = "empty contents"
+        PreviewData = u"(본문이 비어 있습니다)"
     
     # 데이터에서 키워드에 해당하는 곳 불드체로 표시
     #strTitle = strTitle.replace(keyword, "<b>" + keyword + "</b>")
--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
 from . import views
+#from . import search_views
 
 urlpatterns = [
     url(r'^$', views.main_page, name='main_page'),
     url(r'images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': r'C:\Users\bit\Boogle\search\images'}),
+    url(r'^search/(?P<mode>[a-z]+)/(?P<keyword>\S+)/Page(?P<page>\d+)/$', views.SearchPage),
+    url(r'^search/(?P<mode>[a-z]+)/(?P<keyword>\S+)/$', views.SearchPage),
+   
+    
 ]
